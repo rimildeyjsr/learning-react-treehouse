766589633858cff35bd5361c76040f09b25b70af
rimildeyjsr <rimildeyjsr@gmail.com>
2017-01-28 22:24:54 +0530

    player and counter component

--- app.jsx
+++ app.jsx
@@ -10,24 +10,48 @@ Header.propTypes = {
     title: React.PropTypes.string.isRequired,
 };
 
+function Player(props){
+    return (
+        <div className="player">
+            <div className="player-name">
+                {props.name}
+            </div>
+            <div className="player-score">
+                <Counter score={props.score} />
+            </div>
+        </div>
+
+    );
+
+}
+
+Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired
+}
+
+function Counter(props) {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <div className="counter-score"> {props.score} </div>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+Counter.propTypes = {
+    score: React.PropTypes.number.isRequired
+}
+
 function Application(props) {
     //components are created with capitalized names.
     return (
         <div className="scoreboard">
             <Header title="{props.title}" />
             <div className="players">
-                <div className="player">
-                    <div className="player-name">
-                        Rimil Dey
-                    </div>
-                    <div className="player-score">
-                        <div className="counter">
-                            <button className="counter-action decrement"> - </button>
-                            <div className="counter-score"> 31 </div>
-                            <button className="counter-action increment"> + </button>
-                        </div>
-                    </div>
-                </div>
+                <Player name="Rimil Dey" score={18} />
+                <Player name="Prateek Phoenix" score={26} />
             </div>
         </div>
             );
