8225f3aa89dc91cf06640facd2a6ed837053d05a
rimildeyjsr <rimildeyjsr@gmail.com>
2017-01-28 20:04:41 +0530

    first css class added!

--- app.jsx
+++ app.jsx
@@ -1,10 +1,9 @@
 function Application() {
     //components are created with capitalized names.
     return (
-        <div>
-            <h1>Hello from react!</h1>
-            <p>I was rendered from the application component!</p>
-        </div>
+       <div className="application">
+
+       </div>
     );
 }
 
