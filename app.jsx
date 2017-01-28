0bc0db477b815f06bc18433a1b427bfdd20f8d6f
rimildeyjsr <rimildeyjsr@gmail.com>
2017-01-28 20:28:45 +0530

    markup added with first player

--- app.jsx
+++ app.jsx
@@ -1,10 +1,26 @@
 function Application() {
     //components are created with capitalized names.
     return (
-       <div className="application">
-
-       </div>
-    );
+        <div className="application">
+            <div className="header">
+                <h1>ScoreBoard</h1>
+            </div>
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">
+                        Rimil Dey
+                    </div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score"> 31 </div>
+                            <button className="counter-action increment"> + </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            );
 }
 
 
