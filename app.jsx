520f48d1b7f44d3cf9965fc95c0a046963d669a4
rimildeyjsr <rimildeyjsr@gmail.com>
2017-01-28 19:48:16 +0530

    application component renders stuff

--- app.jsx
+++ app.jsx
@@ -1,1 +1,14 @@
-ReactDOM.render(<h1>Hello</h1>,document.getElementById('container'));
+function Application() {
+    //components are created with capitalized names.
+    return (
+        <div>
+            <h1>Hello from react!</h1>
+            <p>I was rendered from the application component!</p>
+        </div>
+    );
+}
+
+
+
+
+ReactDOM.render(<Application/>,document.getElementById('container'));
