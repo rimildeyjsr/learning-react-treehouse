31d751049c8209392d5a7c255f4523260fbad427
rimildeyjsr <rimildeyjsr@gmail.com>
2017-01-29 14:46:18 +0530

    added state to the counter

--- app.jsx
+++ app.jsx
@@ -54,19 +54,28 @@ Player.propTypes = {
     score: React.PropTypes.number.isRequired
 }
 
-function Counter(props) {
-    return (
-        <div className="counter">
-            <button className="counter-action decrement"> - </button>
-            <div className="counter-score"> {props.score} </div>
-            <button className="counter-action increment"> + </button>
-        </div>
-    );
-}
+var Counter = React.createClass({
+    propTypes: {
+        score: React.PropTypes.number.isRequired
+    },
 
-Counter.propTypes = {
-    score: React.PropTypes.number.isRequired
-}
+    getInitialState: function(){
+        return {
+            score: 0,
+        }
+    },
+
+    render: function() {
+        return (
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> {this.state.score} </div>
+                <button className="counter-action increment"> + </button>
+            </div>
+        );
+    }
+});
+
 
 function Application(props) {
     //components are created with capitalized names.
