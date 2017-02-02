5707ba01a7af20deec997fcd2e184c258970255c
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-02 10:47:10 +0530

    can type

--- app.jsx
+++ app.jsx
@@ -22,11 +22,30 @@ var PLAYERS = [
 ];
 
 var AddPlayerForm = React.createClass({
+    propTypes: {
+        onAdd: React.PropTypes.func.isRequired
+    },
+
+    getInitialState: function(){
+        return {
+            name:""
+        };
+    },
+
+    onSubmit: function(e){
+        e.preventDefault();
+        //this.props.onAdd();
+    },
+
+    onNameChange: function(e){
+        this.setState({name: e.target.value});
+    },
+
     render: function(){
         return (
             <div className="add-player-form">
-                <form>
-                    <input type="text"/>
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" value={this.state.name} onChange={this.onNameChange} />
                     <input type="submit" value="Add Player"/>
                 </form>
             </div>
