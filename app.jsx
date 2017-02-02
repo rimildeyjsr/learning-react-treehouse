ce366356477ae1ffb89e13c8fb025288bf49dc58
rimildeyjsr <rimildeyjsr@gmail.com>
2017-02-02 16:13:40 +0530

    start stop interchanging

--- app.jsx
+++ app.jsx
@@ -30,13 +30,29 @@ var Stopwatch = React.createClass({
         }
     },
 
+    onStart: function(){
+        this.setState({ running:true });
+    },
+
+    onStop: function(){
+        this.setState({ running:false });
+    },
+
+    onReset: function(){
+
+    },
+
     render: function(){
         return(
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
                 <div className="stopwatch-time">0</div>
-                {this.state.running ? startStop = <button>Stop</button> : startStop = <button>Start</button>}
-                <button>Reset</button>
+                {this.state.running
+                    ?
+                    <button onClick={this.onStop} >Stop</button>
+                    :
+                    <button onClick={this.onStart} >Start</button>}
+                <button onClick={this.onReset} >Reset</button>
             </div>
         );
     }
